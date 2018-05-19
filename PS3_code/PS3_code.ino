f10d4ef1d3a5fa2810efe59e1dd5bb5f04165fbc
chrissunny94 <chrissunny94@gmail.com>
2018-05-19 21:57:57 +0530

    New changes

---
+++ PS3_code/PS3_code.ino
@@ -0,0 +1,153 @@
+#include <PS3BT.h>
+#include <XBOXUSB.h>
+
+#define TX_INTERVAL 5
+#define RX_TIMEOUT 100
+#define STATUS_BLINKRATE 250
+
+#define deadzone_xbox 15
+#define offset_xbox 7
+#define deadzone_ps3 7
+#define offset_ps3 2
+
+USB Usb;
+BTD Btd(&Usb);
+PS3BT PS3(&Btd);
+
+byte filter[] ={0b10101010,0b01100110,0b10101010};
+byte filtere[]={0b11111110,0b01111110,0b01111111};
+byte vals[]={127,0,0b01000000};
+int pos, negOff, posOff, l2, r2;
+
+int speed_state=0;
+
+double old_tx;
+double old_rx;
+double old_blink;
+
+double avg=4;
+boolean lowbat=false;
+
+int i=0;
+
+/*----------------------------------------Main loop--------------------------------------*/
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  pinMode(7,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  if (Usb.Init() == -1){
+    while(1){
+      Serial.println("USB failed");
+      delay(500);
+    }
+  }
+  analogReadResolution(16);
+}
+
+void loop() {
+  blinkStatus();
+  Usb.Task();
+  getControllerInputs();
+  sendData();
+  if(Serial1.available()>=4){
+    while(Serial1.available())
+      Serial.print(Serial1.read()); 
+    digitalWrite(4,HIGH);
+    old_rx=millis();
+  }
+  if(millis()>old_rx+RX_TIMEOUT)
+     digitalWrite(4,LOW);
+     
+  avg-=(avg-((analogRead(A0)/65535.0)*3.3+1.11))/50;
+  if(avg<=3.6)
+    lowbat=true;
+  if(avg<=3.65)
+    lowbat=false;
+  Serial.println(avg);
+}
+
+/*------------------------------------Main Functions-----------------------------------------*/
+void getControllerInputs(){
+  if(PS3.PS3Connected){ 
+    digitalWrite(7,HIGH);
+    calcPs3Input();
+  }
+  else{
+    digitalWrite(7,LOW);
+    vals[0]=127;
+    vals[1]=0;
+    vals[2]=0b01000000;
+  }
+}
+
+void sendData(){
+  if(millis()>=(old_tx+TX_INTERVAL)){
+    Serial1.write(filter,sizeof(filter));
+    Serial1.write(vals,sizeof(vals));
+    Serial1.write(filtere,sizeof(filtere));
+    old_tx=millis();
+  }
+}
+
+/*-------------------------------------Sub functions--------------------------------------*/
+void blinkStatus(){
+   if(millis()>old_blink+STATUS_BLINKRATE){
+     if(!lowbat)
+       digitalWrite(3,!digitalRead(3));
+     else
+       digitalWrite(3,HIGH);
+     old_blink=millis();
+   }
+}
+
+void calcPs3Input(){
+  pos=PS3.getAnalogHat(LeftHatX)-128;
+  negOff=offset_ps3-deadzone_ps3;
+  posOff=offset_ps3+deadzone_ps3;
+  l2=PS3.getAnalogButton(L2);
+  r2=PS3.getAnalogButton(R2);
+  if(PS3.getButtonClick(START)&&PS3.getButtonPress(L1)&&PS3.getButtonPress(R1)){
+     speed_state++;
+     if(speed_state==3)
+       speed_state=0;
+      
+     PS3.setLedOff(LED3);
+     PS3.setLedOff(LED4);  
+     if(speed_state==1)
+       PS3.setLedOn(LED4);
+     else if(speed_state==2){
+       PS3.setLedOn(LED3);
+       PS3.setLedOn(LED4);
+     }
+     
+  }
+  
+  vals[0]=map(min(pos,negOff),negOff,-128,127,0)+map(max(pos,posOff),posOff,127,0,128); 
+
+  if(l2>0){
+    vals[1]=l2;
+    vals[2]|=0b10000000;
+  }
+  else{
+    switch(speed_state){
+       case 0:
+         vals[1]=r2*0.50;
+         break;
+       case 1:
+         vals[1]=r2*((PS3.getButtonPress(TRIANGLE))? 1:0.70);
+         break;
+       case 2:
+         vals[1]=r2;
+         break; 
+    }
+    vals[2]&=0b01111111;
+  }
+  if(PS3.getButtonPress(CIRCLE))
+    vals[2]|=0b01000000;
+  else
+    vals[2]&=0b10111111;
+}
+
---
+++ Tractor_code/Tractor_code.ino
@@ -0,0 +1,125 @@
+#include <Servo.h>
+
+#define TX_INTERVAL 25
+
+#define commTimeout 125 //ms
+
+#define STEERING_MIN 620 //20
+#define STEERING_MAX 1650 //120
+
+#define steeringOffsetPin A9
+#define steeringPin 4
+#define drivePin 3
+
+Servo steering,drive;
+int steeringOffset;
+int steeringPos;
+int steeringCalc;
+float sFactorMin;
+float sFactorMax;
+int drivePos;
+int driveCalc;
+
+byte aux, rev,brake;
+
+unsigned long timeout;
+
+double old_tx;
+
+boolean datastart=false;
+int bytecount=0;
+byte filter[] ={0b10101010,0b01100110,0b10101010};
+byte filtere[]={0b11111110,0b01111110,0b01111111};
+byte inbuff[3],databuff[20];
+
+
+void initializePins(){
+   drive.attach(drivePin);
+   steering.attach(steeringPin);
+}
+
+/*-------------------------------------------Main loop---------------------------------------*/
+void setup(){
+  initializePins();
+  Serial.begin(115200);
+  Serial1.begin(115200); 
+}
+
+void loop(){
+  getInputs();
+  checkTimeout(); 
+  calcSteering();
+  calcDrive();
+  setOutputs();
+  sendData();
+}
+
+/*-----------------------------------------Main functions---------------------------------*/
+//  127000000
+
+void checkTimeout(){
+  if(millis()>timeout+commTimeout){
+    drivePos=0;
+    brake=0;
+    steeringPos=127;
+  }
+}
+
+void sendData(){
+  if(millis()>=(old_tx+TX_INTERVAL)){
+    Serial1.print("tick");
+    old_tx=millis();
+  }
+}
+
+void getInputs(){
+  byte c;
+  if(Serial1.available())
+    c=Serial1.read();
+   
+  if(datastart){
+    databuff[bytecount++]=c;
+    if(bytecount>=7){
+      datastart=false;
+      if(databuff[3]==filtere[0]&&databuff[4]==filtere[1]&&databuff[5]==filtere[2]){
+        timeout=millis();
+        steeringPos=databuff[0];
+        drivePos=databuff[1];
+        aux=databuff[2];
+        rev=aux&0b10000000;
+        brake=aux&0b01000000;
+      }
+    }  
+  } 
+   
+  inbuff[0]=inbuff[1];
+  inbuff[1]=inbuff[2];
+  inbuff[2]=c;
+  if(filter[0]==inbuff[0]&&filter[1]==inbuff[1]&&filter[2]==inbuff[2]){
+    datastart=true;
+    bytecount=0;
+  }
+}
+
+void calcSteering(){
+  steeringOffset=analogRead(steeringOffsetPin)+725;
+  sFactorMin=(float(STEERING_MAX-STEERING_MIN)/2.0)/(steeringOffset-STEERING_MIN);
+  sFactorMax=(float(STEERING_MAX-STEERING_MIN)/2.0)/(STEERING_MAX-steeringOffset); 
+  steeringCalc=(steeringOffset+(map(steeringPos,0,255,-0.5*(STEERING_MAX-STEERING_MIN),0.5*(STEERING_MAX-STEERING_MIN))/((steeringPos<=127)? sFactorMax:sFactorMin)));
+  steeringCalc=min(STEERING_MAX,max(STEERING_MIN,steeringCalc));
+  //Serial.print(steeringCalc);
+  //Serial.print("   ");
+}
+
+void calcDrive(){
+  driveCalc=1500+(map(drivePos,0,255,0,500)*((rev)?-1:1));
+  if(brake)
+    driveCalc=1400;
+  //Serial.println(driveCalc);
+}
+
+void setOutputs(){
+  //steering.write(steeringCalc);
+  steering.writeMicroseconds(steeringCalc);
+  drive.writeMicroseconds(driveCalc);
+}
