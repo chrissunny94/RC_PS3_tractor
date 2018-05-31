e7f1a4a0fcaeed807a29482aeb27877ac2c2d265
chrissunny94 <chrissunny94@gmail.com>
2018-05-31 20:49:20 +0530

    Added L298 functionality to test the performance

--- PS3_standalone_BT/PS3_standalone_BT.ino
+++ PS3_standalone_BT/PS3_standalone_BT.ino
@@ -8,6 +8,11 @@
 #include <usbhub.h>
 #include <Servo.h>
 
+int left_1 = 6;
+int left_2 = 5;
+int right_1 = 9;
+int right_2 = 3;
+
 Servo left_drive;
 Servo right_drive;
 
@@ -49,7 +54,11 @@ void init_actuators(){
   tilt_bucket.attach(tilt_bucket_pin);
   back_hoe_x.attach(back_hoe_x_pin);
   back_hoe_y.attach(back_hoe_y_pin);
-  
+
+  pinMode(left_1 ,OUTPUT);
+  pinMode(left_2 ,OUTPUT);
+  pinMode(right_1 ,OUTPUT);
+  pinMode(right_2 ,OUTPUT);
   }
 
 void setup() {
@@ -72,102 +81,48 @@ void loop() {
   Usb.Task();
 
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
-    if (PS3.getAnalogHat(LeftHatX) > 137 || PS3.getAnalogHat(LeftHatX) < 117 || PS3.getAnalogHat(LeftHatY) > 137 || PS3.getAnalogHat(LeftHatY) < 117 || PS3.getAnalogHat(RightHatX) > 137 || PS3.getAnalogHat(RightHatX) < 117 || PS3.getAnalogHat(RightHatY) > 137 || PS3.getAnalogHat(RightHatY) < 117) {
-      Serial.print(F("\r\nLeftHatX: "));
-      Serial.print(PS3.getAnalogHat(LeftHatX));
-      Serial.print(F("\tLeftHatY: "));
-      Serial.print(PS3.getAnalogHat(LeftHatY));
-      if (PS3.PS3Connected) { // The Navigation controller only have one joystick
-        Serial.print(F("\tRightHatX: "));
-        Serial.print(PS3.getAnalogHat(RightHatX));
-        Serial.print(F("\tRightHatY: "));
-        Serial.print(PS3.getAnalogHat(RightHatY));
-      }
-    }
-
+    
     // Analog button values can be read from almost all buttons
-    if (PS3.getAnalogButton(L2) || PS3.getAnalogButton(R2)) {
-      Serial.print(F("\r\nL2: "));
-      Serial.print(PS3.getAnalogButton(L2));
-      if (PS3.PS3Connected) {
-        Serial.print(F("\tR2: "));
-        Serial.print(PS3.getAnalogButton(R2));
+    int L_channel = PS3.getAnalogButton(L2);
+    int R_channel = PS3.getAnalogButton(R2);
+    bool L1_Click = PS3.getButtonPress(L1);
+    bool R1_Click = PS3.getButtonPress(R1);
+    if ( PS3.getButtonPress(L2) ) {
+      
+      if (L1_Click )
+        {analogWrite(left_2,L_channel);
+         analogWrite(left_1,0);} 
+      else {
+        analogWrite(left_1,L_channel);
+        analogWrite(left_2,0);
       }
     }
 
-    if (PS3.getButtonClick(PS)) {
-      Serial.print(F("\r\nPS"));
-      PS3.disconnect();
-    }
-    else {
-      if (PS3.getButtonClick(TRIANGLE)) {
-        Serial.print(F("\r\nTraingle"));
-        PS3.setRumbleOn(RumbleLow);
-      }
-      if (PS3.getButtonClick(CIRCLE)) {
-        Serial.print(F("\r\nCircle"));
-        PS3.setRumbleOn(RumbleHigh);
-      }
-      if (PS3.getButtonClick(CROSS))
-        Serial.print(F("\r\nCross"));
-      if (PS3.getButtonClick(SQUARE))
-        Serial.print(F("\r\nSquare"));
+    else if ( PS3.getButtonPress(R2) ) {
 
-      if (PS3.getButtonClick(UP)) {
-        Serial.print(F("\r\nUp"));
-        if (PS3.PS3Connected) {
-          PS3.setLedOff();
-          PS3.setLedOn(LED4);
-        }
+      if (R1_Click){
+            analogWrite(right_1,0);
+            analogWrite(right_2,R_channel);
       }
-      if (PS3.getButtonClick(RIGHT)) {
-        Serial.print(F("\r\nRight"));
-        if (PS3.PS3Connected) {
-          PS3.setLedOff();
-          PS3.setLedOn(LED1);
-        }
-      }
-      if (PS3.getButtonClick(DOWN)) {
-        Serial.print(F("\r\nDown"));
-        if (PS3.PS3Connected) {
-          PS3.setLedOff();
-          PS3.setLedOn(LED2);
-        }
-      }
-      if (PS3.getButtonClick(LEFT)) {
-        Serial.print(F("\r\nLeft"));
-        if (PS3.PS3Connected) {
-          PS3.setLedOff();
-          PS3.setLedOn(LED3);
-        }
-      }
-
-      if (PS3.getButtonClick(L1))
-        Serial.print(F("\r\nL1"));
-      if (PS3.getButtonClick(L3))
-        Serial.print(F("\r\nL3"));
-      if (PS3.getButtonClick(R1))
-        Serial.print(F("\r\nR1"));
-      if (PS3.getButtonClick(R3))
-        Serial.print(F("\r\nR3"));
-
-      if (PS3.getButtonClick(SELECT)) {
-        Serial.print(F("\r\nSelect - "));
-        PS3.printStatusString();
-      }
-      if (PS3.getButtonClick(START)) {
-        Serial.print(F("\r\nStart"));
-        printAngle = !printAngle;
+      else{
+          analogWrite(right_1,R_channel);
+          analogWrite(right_2,0);
       }
     }
-#if 0 // Set this to 1 in order to see the angle of the controller
-    if (printAngle) {
-      Serial.print(F("\r\nPitch: "));
-      Serial.print(PS3.getAngle(Pitch));
-      Serial.print(F("\tRoll: "));
-      Serial.print(PS3.getAngle(Roll));
-    }
-#endif
+    else{
+      analogWrite(left_2,0);
+      analogWrite(left_1,0);
+      analogWrite(right_2,0);
+      analogWrite(right_1,0);
+      }
+
   }
+
+  else{
+      analogWrite(left_2,0);
+      analogWrite(left_1,0);
+      analogWrite(right_2,0);
+      analogWrite(right_1,0);
+      }
 
 }
